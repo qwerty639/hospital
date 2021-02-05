07d940db37572a62a0ff3c586f24e6e74703c8e2
qwerty639 <said.abdurakhmanov.79@mail.ru>
2021-02-05 12:20:18 +0300

    Application

---
+++ HOSPITAL/HOSPITAL/Views/Pages/AddPages/AddTherapyPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HOSPITAL.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddTherapyPage.xaml
+    /// </summary>
+    public partial class AddTherapyPage : Page
+    {
+        public AddTherapyPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMenu_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MenuPage());
+        }
+    }
+}
---
+++ HOSPITAL/HOSPITAL/Views/Pages/MenuPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HOSPITAL.Views.Pages;
+
+namespace HOSPITAL.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MenuPage.xaml
+    /// </summary>
+    public partial class MenuPage : Page
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddPacient_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddPacientPage());
+        }
+
+        private void btnAddDoctor_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddDoctorPage());
+        }
+
+        private void btnTherapy_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddTherapyPage());
+        }
+    }
+}
